28f72b643ad66960ae5c67d696ab132ec10f9f04
Anthony Bosek <anthony.bosek@gmail.com>
2023-12-05 11:17:03 -0700

    server config

--- server/app.py
+++ server/app.py
@@ -1,1 +1,21 @@
-# server/app.py
+#! /usr/bin/env python3
+
+from config import app, db, api
+
+# models
+
+# routes
+
+# schemas
+
+# api resources
+
+
+# views
+@app.route("/")
+def index():
+    return "Fantasy Eleven API"
+
+
+if __name__ == "__main__":
+    app.run(port=5555, debug=True)
--- server/config.py
+++ server/config.py
@@ -1,1 +1,27 @@
-# server/config.py
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_smorest import Api
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config["API_TITLE"] = "Fantasy Eleven API"
+app.config["API_VERSION"] = "v1"
+app.config["OPENAPI_VERSION"] = "3.0.2"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.secret_key = os.environ.get("APP_SECRET")
+CORS(app)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+api = Api(app)
