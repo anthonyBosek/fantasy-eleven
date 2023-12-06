d1551c5d2e804f217f99a0d1b9497f28ca1197a5
Anthony Bosek <anthony.bosek@gmail.com>
2023-12-05 20:18:48 -0700

    matchup schema

---
+++ server/schemas/matchup_schema.py
@@ -0,0 +1,33 @@
+from marshmallow import fields, validate
+from config import ma
+from models.matchup import Matchup
+
+
+class MatchupSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Matchup
+        load_instance = True
+        fields = [
+            "id",
+            "week_number",
+            "league_id",
+            "home_team_id",
+            "away_team_id",
+            "league",
+            "home_team",
+            "away_team",
+            "league_name",
+            "home_team_name",
+            "away_team_name",
+        ]
+
+    week_number = fields.Integer(required=True)
+    league_id = fields.Integer(required=True)
+    home_team_id = fields.Integer(required=True)
+    away_team_id = fields.Integer(required=True)
+    league = fields.String(dump_only=True)
+    home_team = fields.String(dump_only=True)
+    away_team = fields.String(dump_only=True)
+    league_name = fields.String(dump_only=True)
+    home_team_name = fields.String(dump_only=True)
+    away_team_name = fields.String(dump_only=True)
